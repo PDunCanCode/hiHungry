622de1e8b33056f35f148ef052c32d800c939ca8
pDunCanCode <chesiredarling@gmail.com>
2021-02-07 08:18:07 -0800

    add async function requestListener

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,14 @@ import fs from "fs/promises";
 async function requestListener(req: IncomingMessage, res: ServerResponse) {
   const filePath = path.join(__dirname, "static/index.html");
 
-  const data = await fs.readFile(filePath);
+  const data = await fs.readFile(filePath, "utf-8");
+
+  res.writeHead(200, {
+    "Content-type": "text/html",
+    "content-lenght": data.length,
+  });
+  res.write(data);
+  res.end();
 }
 
 http.createServer(requestListener).listen(3000, () => {
