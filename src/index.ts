75870e260811f7d805837557103b902213a8c7c2
pDunCanCode <chesiredarling@gmail.com>
2021-02-07 08:39:05 -0800

    add routes and one pesky 'any' type

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import http, { IncomingMessage, ServerResponse } from "http";
 import path from "path";
 import fs from "fs/promises";
+import url from "url";
 
 async function requestListener(req: IncomingMessage, res: ServerResponse) {
-  const filePath = path.join(__dirname, "static/index.html");
+  const parsedUrl = url.parse(req.url || "");
 
-  const data = await fs.readFile(filePath, "utf-8");
+  let data = "";
+  let statusCode = 200;
+  try {
+    let pathName = parsedUrl.pathname;
+    if (pathName === "/") pathName = "/index";
+    const filePath = path.join(__dirname, `static${pathName}.html`);
 
-  res.writeHead(200, {
+    data = await fs.readFile(filePath, "utf-8");
+  } catch {
+    data = (await fs.readFile(path.join(__dirname, "static/404.html"))) as any;
+    statusCode = 404;
+  }
+
+  res.writeHead(statusCode, {
     "Content-type": "text/html",
-    "content-lenght": data.length,
+    "content-length": data.length,
   });
   res.write(data);
   res.end();
